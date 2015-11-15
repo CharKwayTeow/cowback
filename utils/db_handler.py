13c34a1a1a19a0720eafbefd5502f040e08626e6
Sigma Chen <sigma322@gmail.com>
2015-11-14 22:05:38 -0800

    add db_handler and crawl function

---
+++ utils/db_handler.py
@@ -0,0 +1,23 @@
+import sqlite3
+
+
+def get_news():
+    db = sqlite3.connect('cowback.db')
+    c = db.cursor()
+    c.execute(" SELECT * FROM news ORDER BY ID desc LIMIT 10 ") # only get the last 10 news
+    content = ""
+    for row in c:
+        content += '\n{0}\n{1}\n{2}\n\n'.format(row[1], row[2], row[3]) #row[0] is id
+    db.close()
+    return content
+
+def insert_news(pubdate, title, content):
+    db = sqlite3.connect('cowback.db')
+    c = db.cursor()
+
+    # make sure the table already exists, if not create one
+    c.execute(" CREATE TABLE IF NOT EXISTS news ( id INTEGER PRIMARY KEY AUTOINCREMENT, pubdate DATE, title TEXT, content TEXT) ")
+    
+    c.execute(' INSERT INTO news VALUES (null, ?, ?, ?) ', (pubdate, title, content))
+    db.commit()
+    db.close()
