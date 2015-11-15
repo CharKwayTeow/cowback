9686fa8ddd19add4f7e0f3befd434c84d56ec582
Sigma Chen <sigma322@gmail.com>
2015-11-14 22:11:13 -0800

    add crawl_news func and schedule update

--- services/news_subscriptor.py
+++ services/news_subscriptor.py
@@ -1,3 +1,8 @@
+import logging
+import feedparser
+from time import sleep
+from utils.db_handler import get_news
+from utils.db_handler import insert_news
 from services.service import Service
 from services.subscriptor import Subscriptor
 
@@ -8,3 +13,17 @@ class NewsSubscriptor(Subscriptor):
 
     def process(self, message):
         logging.debug(message)
+
+    def run(self):
+        while True:
+            self.crawl_news()
+            # print(get_news())
+            sleep(10) # in seconds
+
+    # Crawl News website and store into DB
+    def crawl_news(self):
+        rss = 'http://www.voanews.com/api/epiqq'
+        feed = feedparser.parse(rss)
+        for key in feed["entries"]: 
+            insert_news(key["published"],key["title"],key["description"])
+        print("crawl_news done.")
