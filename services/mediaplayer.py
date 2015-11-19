3e890d292ae9337c165f23e5e9a1e4669238d110
enriquedon <enriquedon1992@gmail.com>
2015-11-18 16:54:08 -0800

    keep playing

--- services/mediaplayer.py
+++ services/mediaplayer.py
@@ -1,9 +1,44 @@
 from services.service import Service
+import pygame
+import os
+import re
+import random
+
 
 class MediaPlayer(Service):
     """docstring for MediaPlayer"""
+
     def __init__(self):
         super(MediaPlayer, self).__init__()
+        self.folder_path = "/Users/dy/Downloads/GEM"
+        # self.song_number = 0
 
     def run(self):
         self.logger.info("MediaPlayer is running.")
+        file_list = self.build_file_list()
+        self.play_songs(file_list)
+
+    def build_file_list(self):
+        file_list = []
+        for root, folders, files in os.walk(self.folder_path):
+            folders.sort()
+            files.sort()
+            for filename in files:
+                if re.search(".(wav|flac|m4a|pls|m3u)$", filename) != None:
+                    file_list.append(os.path.join(root, filename))
+        return file_list
+
+    def play_songs(self, file_list):
+        pygame.mixer.init()
+        random.shuffle(file_list)
+        while True:
+            self.play_next_song(file_list)
+            while pygame.mixer.music.get_busy() == True:
+                continue
+
+    def play_next_song(self,file_list):
+        file_list.insert(0, file_list.pop()) # move current song to the back of the list
+        pygame.mixer.music.load(file_list[0])
+        pygame.mixer.music.play()
+        print(len(file_list[0]))
+        print(file_list)
--- services/speech_recognition.py
+++ services/speech_recognition.py
@@ -1,13 +1,97 @@
 from services.service import Service
+import speech_recognition as sr
+from entities.message import Message
 
 class SpeechRecognition(Service):
     """docstring for SpeechRecognition"""
     def __init__(self, handler):
+
         super(SpeechRecognition, self).__init__()
         self.handler = handler
+        self.stopDict=["stop", "cancel", "thank you","enough"]
+        self.ask_dict=["What's", "How", "what","tell me"]
+
+    def _configure(self,r):
+        r.pause_threshold = 0.8
+        r.energy_threshold = 800
 
     def run(self):
-        pass
+        r = sr.Recognizer()
+        self._configure(r)
+        while True:
+            with sr.Microphone() as source:
+                self.logger.info("Say something!")
+                # print("Say something!")
+                r.record(source, duration=2,offset=0)
+            #        print(r.
+
+                audio = r.listen(source)
+                self.logger.info("Start Processing!")
+                # print("Start Processing!")
+                import time
+                start_time = time.time()
+
+        # recognize speech using Google Speech Recognition
+            try:
+            # for testing purposes, we're just using the default API key
+            # to use another API key, use `r.recognize_google(audio, key="GOOGLE_SPEECH_RECOGNITION_API_KEY")`
+            # instead of `r.recognize_google(audio)`
+                self.logger.info("service started.")
+                voice_command= r.recognize_google(audio)
+                self.parse_command(voice_command)
+
+
+            except sr.UnknownValueError:
+                self.logger.warn("Google Speech Recognition could not understand audio")
+                # print("Google Speech Recognition could not understand audio")
+            except sr.RequestError as e:
+                self.logger.warn("Could not request results from Google Speech Recognition service; {0}".format(e))
+            end_time = time.time()
+            # self.logger.info("This Processing totally uses " + (end_time-start_time))
+
+
+
+    def parse_command(self, voice_command):
+        print("Google Speech Recognition thinks you said " + voice_command)
+        self.send(Message('mediaplayer', 'start', None))
+        if any(stop in voice_command for stop in self.stopDict):
+            # if "alarm" or "alarms" in voice_command:
+            #     if "all" in voice_command:
+            #         self.send(Message('mediaplayer', 'start', None))
+            #         print "cancel all alarms"
+            #     else:
+            #         print "cancel time"
+            # else:
+            self.send(Message('All', 'stop', None))
+            print ("stop reading or playing")
+        elif "play" and "music" in voice_command:
+            # self.send(Message('All', 'stop', None))
+            self.send(Message('mediaplayer', 'start', None))
+            print ("start playing music")
+        elif "weather" in voice_command and any(ask in voice_command for ask in self.ask_dict):
+            # self.send(Message('All', 'stop', None))
+            self.send(Message('weather_reporter', 'start', None))
+            print ("today's weather")
+            # if "today" or "this" in voice_command:
+            #     print "today's weather"
+            # else:
+            #     print "next week weather list"
+        # elif "alarm" or "alarms" in voice_command:
+        #     if "set" in voice_command:
+        #         print "time"
+        #     else:
+        #         print "read all alarms"
+        elif "news" in voice_command:
+            # self.send(Message('All', 'stop', None))
+            self.send(Message('news_reporter', 'start', None))
+            print ("start reading news")
+            # if any(ask in voice_command for ask in self.ask_dict):
+            #     print "read news"
+            # else:
+            #     print "Do u wanna today's news?"
+
+        else:
+            print ("Sorry I don't understand")
 
     def send(self, message):
         self.handler.receive(message)
