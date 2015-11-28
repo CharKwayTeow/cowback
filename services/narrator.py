4d13736ffdf0f87456115772049e53930656e943
CharKwayTeow <wug.lim@gmail.com>
2015-11-27 20:00:52 -0800

    Implemented narrator.

--- services/narrator.py
+++ services/narrator.py
@@ -1,6 +1,12 @@
+from gtts import gTTS
+from tempfile import TemporaryFile
 from services.service import Service
 
 class Narrator(Service):
     """docstring for Narrator"""
     def __init__(self):
         super(Narrator, self).__init__()
+
+    def process(self, feed):
+        tts = gTTS(text = feed[2], lang = 'en')
+        tts.save(feed[0] + '/' + feed[1])
