95114c8dd201facc63f4c8382b7c7e5a1d93b759
CharKwayTeow <wug.lim@gmail.com>
2015-11-27 19:05:17 -0800

    Implemented subscriptor.

--- services/subscriptor.py
+++ services/subscriptor.py
@@ -1,9 +1,23 @@
 from services.service import Service
+from services.narrator import Narrator
 
 class Subscriptor(Service):
     """docstring for Subscriptor"""
     def __init__(self):
         super(Subscriptor, self).__init__()
+        self.narrator = Narrator()
 
-    def process(self, message):
+    def run(self):
+        while True:
+            self.crawl()
+            sleep(600) # in seconds
+
+    def crawl(self):
+        feed = feedparser.parse(self.rss)
+        self.process(feed)
+
+    def process(self):
         pass
+
+    def send(self, type, filename, content):
+        self.narrator.receive((type, filename, content))
